a30c8e26314204759f84315255646043e852aca9
JuanMend <juanmend690@gmail.com>
2020-04-21 10:07:55 -0500

    initial commit

---
+++ js/index.js
@@ -0,0 +1,14 @@
+let map;
+let markers = [];
+let infoWindow;
+let locationSelect;
+
+function initMap() {
+	let sydney = { lat: -33.863276, lng: 151.107977 };
+	map = new google.maps.Map(document.getElementById('map'), {
+		center: sydney,
+		zoom: 11,
+		mapTypeId: 'roadmap',
+		mapTypeControlOptions: { style: google.maps.MapTypeControlStyle.DROPDOWN_MENU }
+	});
+}
